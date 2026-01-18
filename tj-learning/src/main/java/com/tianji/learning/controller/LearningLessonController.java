1058991a77b25eb7eb0c14406431d6b3d4944a3d
Orion.Guan <orion.guan@lianwei.com.cn>
2026-01-18 20:11:39 +0800

    feat(learning): 添加学生课程表相关实体和服务
    - 创建 LearningLesson 实体类定义课程表数据结构
    - 实现 ILearningLessonService 接口提供课程表服务
    - 添加 LearningLessonMapper 数据访问接口
    - 创建 LearningLessonController 控制器提供 REST API
    - 实现 LearningLessonServiceImpl 服务逻辑
    - 配置 MyBatis XML 映射文件

---
+++ tj-learning/src/main/java/com/tianji/learning/controller/LearningLessonController.java
@@ -0,0 +1,20 @@
+package com.tianji.learning.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 学生课程表 前端控制器
+ * </p>
+ *
+ * @author Orion.Guan
+ * @since 2026-01-18
+ */
+@RestController
+@RequestMapping("/learning-lesson")
+public class LearningLessonController {
+
+}
