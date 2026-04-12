fb230fda86172c91008b19677abc5171adb73ea5
Orion.Guan <orion.guan@lianwei.com.cn>
2026-04-12 16:15:43 +0800

    refactor(remark): 修改备注服务客户端回退配置
    - 将 fallback 属性替换为 fallbackFactory 属性
    - 使用工厂模式实现更灵活的回退机制
    - 提升服务降级处理的可扩展性

--- tj-api/src/main/java/com/tianji/api/client/remark/RemarkClient.java
+++ tj-api/src/main/java/com/tianji/api/client/remark/RemarkClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Set;
 
-@FeignClient(value = "remark-service",fallback = RemarkClientFallBack.class)
+@FeignClient(value = "remark-service",fallbackFactory = RemarkClientFallBack.class)
 public interface RemarkClient {
 
     /**
